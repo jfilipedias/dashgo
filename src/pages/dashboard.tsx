25a79c42f00fe79c5493859cac82fd1286b9a3d4
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-06-14 20:38:00 -0300

    Creates the dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next'
+import { Header } from '../components/Header'
+
+const Dashboard: NextPage = () => {
+  return (
+    <Header />
+  )
+}
+
+export default Dashboard
